dd623d3ce3eff608b6094c73e977733bca5cc2ba
punisher077 <arthur.mts@gmail.com>
2020-05-31 23:41:10 -0300

    test: 💍 refac tests

--- backend/tests/FilterPostsByUser.test.js
+++ backend/tests/FilterPostsByUser.test.js
@@ -24,29 +24,6 @@ const noPostUser = {
   },
 };
 
-const user2 = {
-  id: 9,
-  name: 'Glenna Reichert',
-  username: 'Delphine',
-  email: 'Chaim_McDermott@dana.io',
-  address: {
-    street: 'Dayna Park',
-    suite: 'Suite 449',
-    city: 'Bartholomebury',
-    zipcode: '76495-3109',
-    geo: {
-      lat: '24.6463',
-      lng: '-168.8889',
-    },
-  },
-  phone: '(775)976-6794 x41206',
-  website: 'conrad.com',
-  company: {
-    name: 'Yost and Sons',
-    catchPhrase: 'Switchable contextually-based project',
-    bs: 'aggregate real-time technologies',
-  },
-};
 const user3 = {
   id: 1,
   name: 'Leanne Graham',
